e4ed627abb7241ceadaafdbaf6d410c5cb773204
ilya-rb <ilya.ryabchuk@gmail.com>
2018-10-31 18:17:26 +0200

    Added google map

--- feature-explore/src/main/java/com/illiarb/tmdbclient/feature/explore/ExploreFragment.kt
+++ feature-explore/src/main/java/com/illiarb/tmdbclient/feature/explore/ExploreFragment.kt
@@ -1,14 +1,64 @@
 package com.illiarb.tmdbclient.feature.explore
 
+import android.os.Bundle
+import android.view.View
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
 import com.illiarb.tmdbclient.feature.explore.di.ExploreComponent
 import com.illiarb.tmdbexplorer.coreui.base.BaseFragment
 import com.illiarb.tmdbexplorerdi.Injectable
 import com.illiarb.tmdbexplorerdi.providers.AppProvider
+import kotlinx.android.synthetic.main.fragment_explore.*
 
 /**
  * @author ilya-rb on 31.10.18.
  */
-class ExploreFragment : BaseFragment<ExploreViewModel>(), Injectable {
+class ExploreFragment : BaseFragment<ExploreViewModel>(), Injectable, OnMapReadyCallback {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        mapView.onCreate(savedInstanceState)
+        mapView.getMapAsync(this)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        mapView.onStart()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mapView.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mapView.onPause()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        mapView.onStop()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mapView.onDestroy()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        mapView.onSaveInstanceState(outState)
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        mapView.onLowMemory()
+    }
+
+    override fun onMapReady(map: GoogleMap) {
+    }
 
     override fun getContentView(): Int = R.layout.fragment_explore
 
