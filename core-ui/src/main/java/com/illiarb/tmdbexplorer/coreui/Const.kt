36ceb49e33a4ae8294e62985580a9ae56a0a07ed
ilya-rb <ilya.ryabchuk@gmail.com>
2018-10-31 14:49:32 +0200

    Cleanup

--- core-ui/src/main/java/com/illiarb/tmdbexplorer/coreui/Const.kt
+++ core-ui/src/main/java/com/illiarb/tmdbexplorer/coreui/Const.kt
@@ -1,7 +1,7 @@
 package com.illiarb.tmdbexplorer.coreui
 
-import android.annotation.SuppressLint
 import java.text.SimpleDateFormat
+import java.util.Locale
 
 /**
  * @author ilya-rb on 26.10.18.
@@ -11,9 +11,9 @@ object Const {
     const val PARSE_DATE_FORMAT = "yyyy-mm-dd"
     const val DISPLAY_DATE_FORMAT = "dd MMMM"
 
-    @SuppressLint("SimpleDateFormat")
-    val PARSE_DATE_SDF = SimpleDateFormat(PARSE_DATE_FORMAT)
+    val PARSE_DATE_SDF: SimpleDateFormat
+        get() = SimpleDateFormat(PARSE_DATE_FORMAT, Locale.getDefault())
 
-    @SuppressLint("SimpleDateFormat")
-    val DISPLAY_DATE_SDF = SimpleDateFormat(DISPLAY_DATE_FORMAT)
+    val DISPLAY_DATE_SDF: SimpleDateFormat
+        get() = SimpleDateFormat(DISPLAY_DATE_FORMAT, Locale.getDefault())
 }
