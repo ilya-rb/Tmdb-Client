634c27e19185bf783808808a7ce6ebb8f97c9b67
ilya-rb <ilya.ryabchuk@gmail.com>
2018-11-20 16:54:38 +0200

    Remove account module

--- feature_dynamic_account/src/main/java/com/illiarb/tmdbclient/dynamic/account/AccountViewModel.kt
+++
@@ -1,9 +0,0 @@
-package com.illiarb.tmdbclient.dynamic.account
-
-import com.illiarb.tmdbexplorer.coreui.viewmodel.BaseViewModel
-import javax.inject.Inject
-
-/**
- * @author ilya-rb on 19.11.18.
- */
-class AccountViewModel @Inject constructor() : BaseViewModel()
