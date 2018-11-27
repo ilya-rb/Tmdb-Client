69d6d929ea2d7389539b19faba7715bf7ceec94d
ilya-rb <ilya.ryabchuk@gmail.com>
2018-11-27 17:48:45 +0200

    Small cache additions

--- storage/src/main/java/com/illiarb/tmdbclient/storage/repositories/MoviesRepositoryImpl.kt
+++ storage/src/main/java/com/illiarb/tmdbclient/storage/repositories/MoviesRepositoryImpl.kt
@@ -4,6 +4,7 @@ import com.illiarb.tmdbclient.storage.R
 import com.illiarb.tmdbclient.storage.local.PersistableStorage
 import com.illiarb.tmdbclient.storage.mappers.MovieMapper
 import com.illiarb.tmdbclient.storage.mappers.ReviewMapper
+import com.illiarb.tmdbclient.storage.model.MovieModel
 import com.illiarb.tmdbclient.storage.network.api.MovieService
 import com.illiarb.tmdblcient.core.entity.Movie
 import com.illiarb.tmdblcient.core.entity.MovieFilter
@@ -12,10 +13,12 @@ import com.illiarb.tmdblcient.core.modules.movie.MoviesRepository
 import com.illiarb.tmdblcient.core.system.ResourceResolver
 import io.reactivex.Single
 import javax.inject.Inject
+import javax.inject.Singleton
 
 /**
  * @author ilya-rb on 25.10.18.
  */
+@Singleton
 class MoviesRepositoryImpl @Inject constructor(
     private val moviesService: MovieService,
     private val persistableStorage: PersistableStorage,
@@ -24,20 +27,39 @@ class MoviesRepositoryImpl @Inject constructor(
     private val resourceResolver: ResourceResolver
 ) : MoviesRepository {
 
-    override fun getMoviesByType(type: String): Single<List<Movie>> =
-        persistableStorage.getMoviesByType(type)
+    private var needsRefresh = true
+
+    override fun getMoviesByType(type: String): Single<List<Movie>> {
+        val onSuccessAction: (List<MovieModel>) -> Unit = {
+            persistableStorage.storeMovies(type, it)
+            needsRefresh = false
+        }
+
+        return persistableStorage.getMoviesByType(type)
             .flatMap { movies ->
-                if (movies.isNotEmpty()) {
-                    Single.just(movies)
-                } else {
+                if (needsRefresh) {
                     moviesService.getMoviesByType(type)
                         .map { it.results }
-                        .doOnSuccess {
-                            persistableStorage.storeMovies(type, it)
+                        .doOnSuccess(onSuccessAction)
+                        .onErrorResumeNext {
+                            if (movies.isNotEmpty()) {
+                                Single.just(movies)
+                            } else {
+                                Single.error(it)
+                            }
                         }
+                } else {
+                    if (movies.isNotEmpty()) {
+                        Single.just(movies)
+                    } else {
+                        moviesService.getMoviesByType(type)
+                            .map { it.results }
+                            .doOnSuccess(onSuccessAction)
+                    }
                 }
             }
             .map(movieMapper::mapList)
+    }
 
     override fun getMovieDetails(id: Int, appendToResponse: String): Single<Movie> =
         moviesService.getMovieDetails(id, appendToResponse).map(movieMapper::map)
