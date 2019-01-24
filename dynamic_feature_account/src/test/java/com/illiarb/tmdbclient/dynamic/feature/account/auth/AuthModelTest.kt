ed048b4c8c33d753c4c6d31ce75bb40ee16fbf17
ilya-rb <ilya.ryabchuk@gmail.com>
2019-01-24 23:58:44 +0200

    Added auth tests

---
+++ dynamic_feature_account/src/test/java/com/illiarb/tmdbclient/dynamic/feature/account/auth/AuthModelTest.kt
@@ -0,0 +1,82 @@
+package com.illiarb.tmdbclient.dynamic.feature.account.auth
+
+import com.illiarb.tmdbclient.dynamic.feature.account.auth.domain.Authenticate
+import com.illiarb.tmdbclient.dynamic.feature.account.auth.presentation.AuthModel
+import com.illiarb.tmdbclient.dynamic.feature.account.auth.presentation.AuthUiState
+import com.illiarb.tmdbcliient.core_test.TestObserver
+import com.illiarb.tmdblcient.core.common.Result
+import com.illiarb.tmdblcient.core.entity.UserCredentials
+import com.illiarb.tmdblcient.core.navigation.AccountScreen
+import com.illiarb.tmdblcient.core.navigation.Router
+import com.nhaarman.mockitokotlin2.mock
+import com.nhaarman.mockitokotlin2.verify
+import kotlinx.coroutines.runBlocking
+import org.junit.After
+import org.junit.Assert.assertEquals
+import org.junit.Before
+import org.junit.Test
+import org.mockito.Mockito
+
+/**
+ * 1. Test on authenticating Loading is showing and auth button is disabled
+ * 2. Test on auth failure errors is showing
+ * 3. Test on auth success navigated to account
+ *
+ * @author ilya-rb on 24.01.19.
+ */
+class AuthModelTest {
+
+    private val authenticate = mock<Authenticate>()
+    private val router = mock<Router>()
+
+    private val authModel = AuthModel(authenticate, router)
+
+    private val testObserver = TestObserver<AuthUiState>()
+
+    @Before
+    fun beforeEachTest() {
+        authModel.stateObservable().addObserver(testObserver)
+    }
+
+    @After
+    fun afterEachTest() {
+        testObserver.clearValues()
+        authModel.stateObservable().removeObserver(testObserver)
+    }
+
+    @Test
+    fun `on start authenticate progress is showing and button is disabled`() {
+
+    }
+
+    @Test
+    fun `on auth failure errors are showing`() {
+
+    }
+
+    @Test
+    fun `on auth success progress is hidden and user navigates to account`() {
+        runBlocking {
+            val credentials = UserCredentials("username", "password")
+
+            Mockito
+                .`when`(authenticate.executeAsync(credentials))
+                .thenReturn(Result.Success(Unit))
+
+            authModel.authenticate(credentials.username, credentials.password)
+
+            testObserver
+                // Idle + Loading + Result state
+                .assertValuesCount(3)
+                .withPrevious {
+                    assertEquals(true, it.isLoading)
+                    assertEquals(false, it.authButtonEnabled)
+                }
+                .withLatest {
+                    assertEquals(false, it.isLoading)
+                }
+
+            verify(router).navigateTo(AccountScreen)
+        }
+    }
+}
