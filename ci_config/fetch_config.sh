30c762a39821e41f5b74c298e462cff010f35e99
ilya-rb <ilya.ryabchuk@gmail.com>
2019-01-31 23:04:19 +0200

    Updated config

--- ci_config/fetch_config.sh
+++ ci_config/fetch_config.sh
@@ -3,6 +3,6 @@
 curl -X POST https://content.dropboxapi.com/2/files/download_zip \
     --header "Authorization: Bearer $DROPBOX_KEY" \
     --header "Dropbox-API-Arg: {\"path\": \"/$CONFIG_ARCHIVE_NAME\"}" \
-    -o "./$CONFIG_ARCHIVE_NAME.zip" \
+    -o "./code/$CONFIG_ARCHIVE_NAME.zip" \
     && unzip -o "$CONFIG_ARCHIVE_NAME.zip" \
     && rm "$CONFIG_ARCHIVE_NAME.zip"
