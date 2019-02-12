9b3b89e141f08fcb87ed3725b269e3481c9d82db
ilya-rb <ilya.ryabchuk@gmail.com>
2019-02-12 16:33:58 +0200

    Added Android App Shortcuts

--- app/src/main/java/com/illiarb/tmdbclient/main/MainActivity.kt
+++ app/src/main/java/com/illiarb/tmdbclient/main/MainActivity.kt
@@ -1,15 +1,18 @@
 package com.illiarb.tmdbclient.main
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.illiarb.tmdbclient.R
 import com.illiarb.tmdbclient.main.di.MainComponent
-import com.illiarb.tmdblcient.core.navigation.Navigator
-import com.illiarb.tmdblcient.core.navigation.NavigatorHolder
 import com.illiarb.tmdbexplorer.coreui.ext.setVisible
 import com.illiarb.tmdbexplorer.coreui.keyboard.KeyboardContentResizer
 import com.illiarb.tmdblcient.core.di.Injectable
 import com.illiarb.tmdblcient.core.di.providers.AppProvider
+import com.illiarb.tmdblcient.core.navigation.Navigator
+import com.illiarb.tmdblcient.core.navigation.NavigatorHolder
+import com.illiarb.tmdblcient.core.navigation.Router
+import com.illiarb.tmdblcient.core.navigation.SearchScreen
 import com.illiarb.tmdblcient.core.tools.ConnectivityStatus
 import com.illiarb.tmdblcient.core.tools.ConnectivityStatus.ConnectionState
 import com.illiarb.tmdblcient.core.util.observable.Observer
@@ -27,6 +30,9 @@ class MainActivity : AppCompatActivity(), Injectable, Observer<ConnectionState> 
     @Inject
     lateinit var connectivityStatus: ConnectivityStatus
 
+    @Inject
+    lateinit var router: Router
+
     override fun inject(appProvider: AppProvider) =
         MainComponent.get(appProvider, this).inject(this)
 
@@ -35,6 +41,8 @@ class MainActivity : AppCompatActivity(), Injectable, Observer<ConnectionState> 
         setContentView(R.layout.activity_main)
 
         KeyboardContentResizer.listen(this)
+
+        checkShortcutLaunch()
     }
 
     override fun onResumeFragments() {
@@ -49,7 +57,22 @@ class MainActivity : AppCompatActivity(), Injectable, Observer<ConnectionState> 
         connectivityStatus.connectionState().removeObserver(this)
     }
 
+    override fun onNewIntent(intent: Intent?) {
+        super.onNewIntent(intent)
+        checkShortcutLaunch()
+    }
+
     override fun onNewValue(value: ConnectionState) {
         connectionStatusLabel.setVisible(value == ConnectionState.NOT_CONNECTED)
+    }
+
+    private fun checkShortcutLaunch() {
+        if (intent.action == INTENT_ACTION_SHORTCUT_SEARCH) {
+            router.navigateTo(SearchScreen)
+        }
+    }
+
+    companion object {
+        const val INTENT_ACTION_SHORTCUT_SEARCH = "search"
     }
 }
