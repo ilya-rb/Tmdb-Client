b79d7b426d3637d974a8b80e3a50efaac8dba465
ilya-rb <ilya.ryabchuk@gmail.com>
2019-09-29 19:15:50 +0300

    Added top overlay

--- storage/src/main/java/com/illiarb/tmdbclient/storage/network/request/CreateSessionRequest.kt
+++
@@ -1,8 +0,0 @@
-package com.illiarb.tmdbclient.storage.network.request
-
-import com.google.gson.annotations.SerializedName
-
-/**
- * @author ilya-rb on 21.11.18.
- */
-data class CreateSessionRequest(@SerializedName("request_token") val requestToken: String)
