bf11609102d87ee045600312c30cc3a581634e8a
ilya-rb <ilya.ryabchuk@gmail.com>
2019-09-29 21:17:34 +0300

    Removed old modules

--- dynamic_feature_account/src/main/java/com/illiarb/tmdbclient/dynamic/feature/account/profile/presentation/ShowSignOutDialog.kt
+++
@@ -1,8 +0,0 @@
-package com.illiarb.tmdbclient.dynamic.feature.account.profile.presentation
-
-import com.illiarb.tmdbexplorer.coreui.uiactions.UiAction
-
-/**
- * @author ilya-rb on 18.01.19.
- */
-object ShowSignOutDialog : UiAction
--- feature_search/src/main/java/com/illiarb/tmdbclient/feature/search/filters/SearchFiltersFragment.kt
+++
@@ -1,3 +0,0 @@
-package com.illiarb.tmdbclient.feature.search.filters
-
-class SearchFiltersFragment
