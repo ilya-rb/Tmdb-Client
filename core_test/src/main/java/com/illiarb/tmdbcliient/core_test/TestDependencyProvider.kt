0ef3249d486a8d3311a45118e251680c40c87b5d
ilya-rb <ilya.ryabchuk@gmail.com>
2020-01-06 01:10:27 +0200

    Test fixes

--- core_test/src/main/java/com/illiarb/tmdbcliient/core_test/TestDependencyProvider.kt
+++ core_test/src/main/java/com/illiarb/tmdbcliient/core_test/TestDependencyProvider.kt
@@ -16,6 +16,9 @@ import com.illiarb.tmdblcient.core.navigation.Router
 import com.illiarb.tmdblcient.core.services.TmdbService
 import com.illiarb.tmdblcient.core.services.analytics.AnalyticsService
 import com.illiarb.tmdblcient.core.storage.ResourceResolver
+import com.illiarb.tmdblcient.core.storage.WorkManager
+import com.illiarb.tmdblcient.core.storage.WorkRequestCreator
+import com.illiarb.tmdblcient.core.storage.WorkerCreator
 import com.illiarb.tmdblcient.core.tools.ConnectivityStatus
 import com.illiarb.tmdblcient.core.tools.DispatcherProvider
 
@@ -35,4 +38,12 @@ object TestDependencyProvider : StorageProvider, ToolsProvider, AnalyticsProvide
     override fun provideConnectivityStatus(): ConnectivityStatus = TestConnectivityStatus()
 
     override fun provideAnalyticsService(): AnalyticsService = TestAnalyticsService()
+
+    override fun provideConfigurationFetchWorker(): WorkManager.Worker {
+        return object : WorkManager.Worker {
+            override fun isWorkerSuitable(workerClassName: String): Boolean = false
+            override val workCreator: WorkerCreator get() = { _, _ -> TODO() }
+            override val workRequestCreator: WorkRequestCreator get() = { TODO() }
+        }
+    }
 }
