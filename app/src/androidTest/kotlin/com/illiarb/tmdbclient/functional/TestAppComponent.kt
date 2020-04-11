615341f3058ab2d61ae29cbbda3e46da9f8b2561
ilya-rb <ilya.ryabchuk@gmail.com>
2020-04-11 23:27:24 +0300

    Fixed build errors

--- app/src/androidTest/kotlin/com/illiarb/tmdbclient/functional/TestAppComponent.kt
+++ app/src/androidTest/kotlin/com/illiarb/tmdbclient/functional/TestAppComponent.kt
@@ -2,12 +2,12 @@ package com.illiarb.tmdbclient.functional
 
 import com.illiarb.tmdbclient.App
 import com.illiarb.tmdbclient.di.AppComponent
+import com.illiarb.tmdbclient.libs.test.TestDependencies
+import com.illiarb.tmdbclient.libs.tools.di.ToolsProvider
 import com.illiarb.tmdbclient.navigation.ActionsBuffer
 import com.illiarb.tmdbclient.navigation.AppRouter
 import com.illiarb.tmdbclient.navigation.NavigatorHolder
 import com.illiarb.tmdbclient.navigation.Router
-import com.illiarb.tmdbclient.libs.tools.di.ToolsProvider
-import com.illiarb.tmdbclient.libs.test.TestDependencies
 import com.illiarb.tmdbclient.services.analytics.di.AnalyticsProvider
 import com.illiarb.tmdbclient.services.tmdb.di.TmdbProvider
 
@@ -23,7 +23,5 @@ class TestAppComponent : AppComponent,
 
   override fun router(): Router = router
 
-  override fun inject(app: App) {
-    app.appInitializers = emptySet()
-  }
+  override fun inject(app: App) = Unit
 }
