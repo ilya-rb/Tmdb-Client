3709ee0e055ef6840532adc40d3841095146ac00
ilya-rb <ilya.ryabchuk@gmail.com>
2020-05-23 12:53:03 +0300

    Fixed test package

--- app/src/androidTest/kotlin/com/illiarb/tmdbclient/functional/TestApplication.kt
+++ app/src/androidTest/kotlin/com/illiarb/tmdbclient/functional/TestApplication.kt
@@ -4,12 +4,12 @@ import android.app.Application
 import androidx.core.provider.FontRequest
 import androidx.emoji.text.EmojiCompat
 import androidx.emoji.text.FontRequestEmojiCompatConfig
-import com.illiarb.tmdbclient.libs.ui.R
 import com.illiarb.tmdbclient.di.AppInjector
+import com.illiarb.tmdbclient.libs.ui.R
 
 class TestApplication : Application() {
 
-  private val testAppComponent = TestAppComponent()
+  private val testAppComponent = TestAppComponent(this)
 
   override fun onCreate() {
     super.onCreate()
