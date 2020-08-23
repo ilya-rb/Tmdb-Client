5ce85d12ed4c87890bf453d03ab0222ae5669249
ilya-rb <ilya.ryabchuk@gmail.com>
2020-08-23 12:53:49 +0300

    Move google services to release folder

--- ci-config/fetch-config.sh
+++ ci-config/fetch-config.sh
@@ -7,5 +7,5 @@ curl -X POST https://content.dropboxapi.com/2/files/download_zip \
     && unzip -o "$CONFIG_ARCHIVE_NAME.zip" \
     && mv $CONFIG_ARCHIVE_NAME/api-config.properties ./ \
     && mv $CONFIG_ARCHIVE_NAME/app/src/debug/google-services.json ./app/src/debug \
-    && mv $CONFIG_ARCHIVE_NAME/app/src/main/google-services.json ./app/src/main \
+    && mv $CONFIG_ARCHIVE_NAME/app/src/release/google-services.json ./app/src/main \
     && rm "$CONFIG_ARCHIVE_NAME.zip"
